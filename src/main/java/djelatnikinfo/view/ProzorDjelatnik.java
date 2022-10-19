26eae1ffca048094823bd914fb13297163865593
Alen Boric <boricalen@live.com>
2022-10-19 16:34:35 +0200

    DjelatnikInfo

--- src/main/java/djelatnikinfo/view/ProzorDjelatnik.java
+++ src/main/java/djelatnikinfo/view/ProzorDjelatnik.java
@@ -37,7 +37,7 @@ public class ProzorDjelatnik extends javax.swing.JFrame {
 
     private void postavke() {
         setTitle(Pomocno.NAZIV_APLIKACIJE + " Djelatnici");
-        btnObrisi.setVisible(true);
+        btnObrisi.setVisible(false);
         prilagodiDatePicker();
 
     }
@@ -365,12 +365,12 @@ public class ProzorDjelatnik extends javax.swing.JFrame {
         txtZavrsenoZanimanje.setText(s.getZavrsenoZanimanje());
 
         
-        btnObrisi.setVisible(s.getMobiteli() == null || s.getMobiteli().isEmpty());  
-        btnObrisi.setVisible(s.getSanitarneiskaznice() == null || s.getSanitarneiskaznice().isEmpty());
-        btnObrisi.setVisible(s.getDjelatniciNaEdukacijama() == null || s.getDjelatniciNaEdukacijama().isEmpty());
-        btnObrisi.setVisible((s.getMobiteli() == null || s.getMobiteli().isEmpty()) && 
-                (s.getSanitarneiskaznice() == null || s.getSanitarneiskaznice().isEmpty())&& 
-                (s.getDjelatniciNaEdukacijama() == null || s.getDjelatniciNaEdukacijama().isEmpty()));
+       // btnObrisi.setVisible(s.getMobiteli() == null || s.getMobiteli().isEmpty());  
+       // btnObrisi.setVisible(s.getSanitarneiskaznice() == null || s.getSanitarneiskaznice().isEmpty());
+       // btnObrisi.setVisible(s.getDjelatniciNaEdukacijama() == null || s.getDjelatniciNaEdukacijama().isEmpty());
+       btnObrisi.setVisible((s.getMobiteli() == null || s.getMobiteli().isEmpty()) && 
+                           (s.getSanitarneiskaznice() == null || s.getSanitarneiskaznice().isEmpty())&& 
+                           (s.getDjelatniciNaEdukacijama() == null || s.getDjelatniciNaEdukacijama().isEmpty()));
     }
 
 
